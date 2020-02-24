bb788d81763929d491d23a5563260ef30d6322e8
tallison <tallison@apache.org>
2020-02-24 11:07:09 -0500

    initial commit

---
+++ src/main/java/org/tallison/xmp/DoNothing.java
@@ -0,0 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tallison.xmp;
+
+/**
+ * Empty class to trigger javadocs so that we can push this to
+ * sonatype's open source repo for eventual publication in maven central
+ * <p>
+ *     This is a repackaging of xmpcore that shade/relocates 'com.adobe.internal' to 'com.adobe'.
+ * </p>
+ */
+public class DoNothing {
+}
